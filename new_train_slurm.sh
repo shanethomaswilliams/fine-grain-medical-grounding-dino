13325a8f56922bb3fa30f6a6125b15472b1308d9
MarkerPretzel <marco.pretell2@gmail.com>
2024-11-20 13:22:58 -0500

    changed gpu mem and path stuff

---
+++ new_train_slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+PARTITION=$1
+GPUS=$2
+GPUS_PER_NODE=$(($2<8?$2:8))
+CPUS_PER_TASK=${CPUS_PER_TASK:-4}
+CFG=$3
+DATASETS=$4
+OUTPUT_DIR=$5
+
+srun -p ${PARTITION} \
+    --job-name=open_G_dino \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --cpus-per-task=${CPUS_PER_TASK} \
+    --mem=32G \
+    --time=24:00:00 \
+    --kill-on-bad-exit=1 \
+    python -u main.py \
+    --output_dir ${OUTPUT_DIR} \
+    -c ${CFG} \
+    --datasets ${DATASETS} \
+    --pretrain_model_path /cluster/tufts/cs152l3dclass/mprete01/Medical-Grounding-DINO/dino_weights/groundingdino_swint_ogc.pth \
+    --options text_encoder_type=/cluster/tufts/cs152l3dclass/mprete01/Medical-Grounding-DINO/bert_weights
